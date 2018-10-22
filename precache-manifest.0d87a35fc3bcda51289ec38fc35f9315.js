f5e7b00504e88390cf643998ef7929e81e49bc34
enducker <davidkuiper@gmail.com>
2018-10-21 21:46:13 -0400

    Updates

---
+++ precache-manifest.0d87a35fc3bcda51289ec38fc35f9315.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e79a5c9c8039fc95d090ad40e32ba0fc",
+    "url": "/MyFactomWallet/static/media/headerLogo.e79a5c9c.png"
+  },
+  {
+    "revision": "2aa7f78ce3cfadc9e58a",
+    "url": "/MyFactomWallet/static/js/runtime~main.2aa7f78c.js"
+  },
+  {
+    "revision": "6e712a10638a400af55d",
+    "url": "/MyFactomWallet/static/js/main.6e712a10.chunk.js"
+  },
+  {
+    "revision": "163ce5d1b5488c623a3c",
+    "url": "/MyFactomWallet/static/js/1.163ce5d1.chunk.js"
+  },
+  {
+    "revision": "6e712a10638a400af55d",
+    "url": "/MyFactomWallet/static/css/main.566c9c85.chunk.css"
+  },
+  {
+    "revision": "594e7f3372938012838d1e5834efbc7a",
+    "url": "/MyFactomWallet/index.html"
+  }
+];
