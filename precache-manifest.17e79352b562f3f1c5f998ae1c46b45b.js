41d4fd0b4e9f4ff26cad60223e39dd33ea8cb801
enducker <davidkuiper@gmail.com>
2018-10-23 20:02:04 -0400

    Updates

---
+++ precache-manifest.17e79352b562f3f1c5f998ae1c46b45b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e79a5c9c8039fc95d090ad40e32ba0fc",
+    "url": "/MyFactomWallet/static/media/headerLogo.e79a5c9c.png"
+  },
+  {
+    "revision": "2aa7f78ce3cfadc9e58a",
+    "url": "/MyFactomWallet/static/js/runtime~main.2aa7f78c.js"
+  },
+  {
+    "revision": "99c6e86294a1aadd50bf",
+    "url": "/MyFactomWallet/static/js/main.99c6e862.chunk.js"
+  },
+  {
+    "revision": "bc3c09c451acfc01e8f1",
+    "url": "/MyFactomWallet/static/js/1.bc3c09c4.chunk.js"
+  },
+  {
+    "revision": "99c6e86294a1aadd50bf",
+    "url": "/MyFactomWallet/static/css/main.5c2da87d.chunk.css"
+  },
+  {
+    "revision": "5c14b0d66fea2bda68f2c92cc99e474f",
+    "url": "/MyFactomWallet/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/MyFactomWallet/precache-manifest.030f5241ab6996116f582d1cff6cd3cd.js"
+  "/MyFactomWallet/precache-manifest.17e79352b562f3f1c5f998ae1c46b45b.js"
 );
 
 workbox.clientsClaim();
